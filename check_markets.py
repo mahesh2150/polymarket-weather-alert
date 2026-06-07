12f22cee143087d2a6420e19702e51754752dce6
mahesh2150 <mahesh2150@gmail.com>
2026-06-07 13:10:09 +0530

    Update check_markets.py

--- check_markets.py
+++ check_markets.py
@@ -1,1 +1,16 @@
-print("VERSION 999")
+import requests
+import json
+
+url = "https://gamma-api.polymarket.com/markets"
+
+response = requests.get(url, timeout=30)
+
+print("Status:", response.status_code)
+
+data = response.json()
+
+print("Markets Found:", len(data))
+
+print("\nFIRST MARKET:\n")
+
+print(json.dumps(data[0], indent=2))
