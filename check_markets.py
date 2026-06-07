3a2f563f9937f7f9c4925e42fab1ac3be935e5de
mahesh2150 <mahesh2150@gmail.com>
2026-06-07 12:46:24 +0530

    Update check_markets.py

--- check_markets.py
+++ check_markets.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 print("Checking Polymarket...")
 
@@ -11,3 +12,6 @@ print("Status Code:", response.status_code)
 data = response.json()
 
 print("Events Found:", len(data))
+
+print("\nFIRST EVENT:\n")
+print(json.dumps(data[0], indent=2))
